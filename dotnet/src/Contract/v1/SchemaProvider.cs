3d0e7657fd01bb040ab3564b87e9159475d5f84a
keinich <rafael.weschle@gmail.com>
2024-09-27 19:38:47 +0200

    packge updates + SchemaProvider #480

---
+++ dotnet/src/Contract/v1/SchemaProvider.cs
@@ -0,0 +1,14 @@
+﻿using MedicalResearch.StudyManagement.Model;
+using System.Data.Fuse;
+using System.Data.ModelDescription;
+
+namespace MedicalResearch.StudyManagement.v1 {
+  public static class SchemaProvider {
+    public static SchemaRoot GetSchemaRoot() {
+      return ModelReader.GetSchema(
+          typeof(ResearchStudy).Assembly,
+          new string[] { typeof(ResearchStudy).Name, typeof(Institute).Name, typeof(DataEndpoint).Name }
+        );
+    }
+  }
+}
